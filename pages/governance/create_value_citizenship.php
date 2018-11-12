84938d9a7f8b015317992470f9245149542d50f2
daneg2 <dane@g2design.co.za>
2018-11-12 16:53:28 +0200

    [Dev] Content population
    [Dev] Content population

--- pages/governance/create_value_citizenship.php
+++ pages/governance/create_value_citizenship.php
@@ -9,7 +9,7 @@
             <div class="col-9 col-lg-10 side-padding">
                 <h1 class="text-uppercase margin-bottom-40px">Creating value through good citizenship</h1>
                 <div class="row margin-bottom-40px">
-                    <div class="col-10 col-lg-8 text-center">
+                    <div class="col-10 offset-1 col-lg-8 offset-lg-2 text-center">
                         <div class="dash"></div>
                         <img class="margin-bottom-20px" src="assets/img/about/p3_ftse4good_logo.png" alt="FTSE4Good Index" style="max-width: 120px;">
                         <h3>Constituent of the <br><b class="larger">FTSE4Good Index</b>, <br>recognising strong environmental, social and governance practices measured against global standards</h3>
@@ -20,10 +20,12 @@
                         <div class="dash margin-bottom-40px"></div>
                     </div>
                 </div>
-                <h2 class="heading-1 text-uppercase">Broadening access to <span class="sky-blue larger xl">healthcare</span></h2>
+
                 <div class="text-center">
+                    <h2 class="heading-1 text-uppercase">Broadening access to <span class="sky-blue larger xl"><br>healthcare</span></h2>
                     <h3 class="margin-bottom-40px"><em>Clicks Group supports the national healthcare agenda of making medicine more affordable and accessible for all South Africans. The group is actively building capacity through training and financial support to address the critical shortage of pharmacists.</em></h3>
                 </div>
+
                 <div class="row margin-bottom-40px">
                     <div class="col-4 text-center">
                         <h3><span class="larger xl green">65 million</span> <br>units of medicine sold in 2018</h3>
@@ -39,9 +41,11 @@
                         <h3><b class="heading-2 green">203 clinics</b> in Clicks pharmacies provide primary care health services</h3>
                     </div>
                 </div>
+
                 <img class="float-left" src="assets/img/governance/p61_pharmacy_icon.png" alt="Pharmacy" style="width: 60px;">
                 <h3 class="margin-bottom-20px"><span class="larger xl green">2 818</span> pharmacy and clinic professionals across&nbsp;Clicks</h3>
                 <h2 class="green margin-bottom-40px"><b class="green">Largest employer</b> of pharmacy staff in the private sector</h2>
+
                 <div class="row">
                     <div class="col-12 col-lg-6 d-flex margin-bottom-40px">
                         <div class="light-blue-bg box-padding dark-blue-border-bottom blue-header">
@@ -62,9 +66,10 @@
                         </div>
                     </div>
                 </div>  
-                <div class="dash margin-bottom-40px"></div>
-                <h2 class="heading-1 text-uppercase">Investing in our <span class="sky-blue larger xl">people</span></h2>
+
                 <div class="text-center margin-bottom-40px">
+                    <div class="dash margin-bottom-40px"></div>
+                    <h2 class="heading-1 text-uppercase">Investing in our <span class="sky-blue larger xl"><br>people</span></h2>
                     <h3 class="margin-bottom-40px"><em>The group is committed to the ongoing investment in its people through training and development, transformation, empowerment, competitive remuneration and incentive schemes, career path planning and by creating a stimulating working environment.</em></h3>
                     <div class="light-blue-bg box-padding medium-content-padding share-ownership blue-header">
                         <h2 class="heading-3 white text-uppercase dark-blue-bg heading">Employee share ownership plan</h2>
@@ -89,6 +94,7 @@
                         </div>
                     </div>
                 </div>  
+
                 <div class="row remove-flex width-100 margin-bottom-40px">
                     <div class="ol-12 col-lg-4 text-center d-flex flex-column align-items-center justify-content-center dark-blue-border-top dark-blue-border-bottom small-content-padding">
                         <h3><span class="larger xl green">15 067</span> <br><b>employees</b> across retail stores, distribution centres and head office</h4>
@@ -102,6 +108,7 @@
                         <h3><span class="larger xl blue">974</span> <br><b>jobs created</b> over the past two years</h3>
                     </div>
                 </div>
+
                 <div class="blue-header text-center box-padding employment-equity">
                     <h2 class="heading-3 white text-uppercase dark-blue-bg heading">Employment equity and diversity</h2>
                     <h3 class="margin-bottom-20px">Clicks Group is committed to creating a diverse workforce by attracting and developing previously disadvantaged people, women and employees with disabilities.</h3>
@@ -110,12 +117,49 @@
                     <h3 class="heading-1 sky-blue d-flex d-lg-block margin-bottom-40px">black directors <b class="white sky-blue-bg box-padding">44%</b> female directors</h3>
                     <h3><b>Board race and gender diversity policy targets exceeded</b></h3>
                 </div>
+
                 <div class="dash margin-bottom-40px"></div>
-                <div class="light-blue-bg box-padding medium-content-padding blue-header text-center">
+                <div class="light-blue-bg box-padding medium-content-padding blue-header text-center dark-blue-border-bottom margin-bottom-40px">
                     <h2 class="heading-3 white text-uppercase dark-blue-bg heading">Skills development</h2>
                     <h3 class="heading-2 margin-bottom-20px"><b>R125 million</b> invested in training and development in 2018</h3>
-                    <img class="margin-top-20px" src="assets/img/governance/p63_skills_development_graph.svg" alt="Skills development" style="width: 700px;">
+                    <img class="margin-bottom-20px" src="assets/img/governance/p63_skills_development_graph.svg" alt="Skills development" style="width: 700px;">
+                    <div class="row">
+                        <div class="col-6 dark-blue-border-right text-right">
+                            <h3>122 trainee managers registered in store operations learnership programme in 2018</h3>
+                        </div>
+                        <div class="col-6 text-left">
+                            <h3 class="green">20 graduates enrolled on the retail graduate development programme</h3>
+                        </div>
+                    </div>
                 </div>
+
+                <div class="text-center">
+                    <h2 class="heading-1 text-uppercase">Investing in our <span class="sky-blue larger xl"><br>community</span></h2>
+                    <h3 class="margin-bottom-20px">Clicks Group continues to demonstrate its commitment to making a sustainable contribution to the communities in which it trades. This is achieved through both enterprise development, by accelerating the sustainability and financial independence of black enterprises, as well as social investment through financial and product donations to non-profit organisations and initiatives.</h3>
+                    <h3><span class="green larger xl">R18.4 million</span> <br>invested in <b>socio-economic development</b> projects aligned to the group’s focus on health and well-being</h3>
+                    <div class="dash"></div>
+                </div>
+
+                <div class="light-blue-bg box-padding medium-content-padding blue-header text-center margin-bottom-40px helping-hand">
+                    <img src="" alt="">
+                    <h2 class="heading-3 white text-uppercase dark-blue-bg heading">Clicks helping hand trust</h2>
+                    <h3 class="margin-bottom-20px">The trust aims to benefit the lives of ordinary South Africans by offering free preventative testing and wellness services through the footprint of over 200 Clicks clinics countrywide.</h3>
+                    <div class="row">
+                        <div class="col-6 dark-blue-border-right">
+                            <h3><b>212 000</b> lives positively impacted over past six years</h3>
+                            <h3><b>Mom and baby</b> wellness and family services have reached over 92 000 families</h3>
+                            <h3><b>HIV testing</b> provided to over 58 000 South Africans through a campaign to encourage individuals to know their HIV status</h3>
+                        </div>
+                        <div class="col-6">
+                            <h3><b>Heart disease</b> testing for more than 82 000 people to encourage healthier lifestyle choices</h3>
+                            <h3><b>Diabetes</b> testing and education for over 72 000 people on the prevention and early detection of the disease</h3>
+                            <h3><b>Girls on the Go</b> campaign has provided reusable and washable sanitary pads to over 18 000 schoolgirls</h3>
+                        </div>
+                    </div>
+                </div>
+                <img class="float-left" src="assets/img/play.png" alt="Play">
+                <h5 class="padding-top-5px">View Clicks Helping Hand Trust video at <u><a href="https://www.clicksgroup.co.za/" target="_blank">www.clicksgroup.co.za</a></u></h5> 
+                <div class="dash"></div>
             </div>
         </div>
     </div>
